17f5b5eaed315dc92e6b21c060b2aaa75ba31003
vanerk03 <vanerk4@gmail.com>
2021-11-08 20:29:22 +0300

    A changed

--- Сhampionship/A.java
+++ Сhampionship/A.java
@@ -1,5 +1,16 @@
+import java.util.Scanner;
+
 public class A {
     public static void main(String[] args) {
+        int a;
+        int b;
+        int n;
+        Scanner sc = new Scanner(System.in);
+        a = sc.nextInt();
+        b = sc.nextInt();
+        n = sc.nextInt();
+        sc.close();
+        System.out.println(2 * ((n - a - 1) / (b - a)) + 1);
         
     }
 }
